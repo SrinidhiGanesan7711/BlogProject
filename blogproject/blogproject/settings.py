7cec54954cf754a4c5ddee28f45934efffed94a9
SrinidhiGanesan <srinidhiganesan7711@gmail.com>
2025-05-08 00:11:37 +0530

    Add settings.py file

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': 'BlogProject',
         'USER': 'postgres',
         'PASSWORD': 'tiger',
-        'HOST': 'localhost',  
+        'HOST': '127.0.0.1',  
         'PORT': '5432',       
     }
 }
